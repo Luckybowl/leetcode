5a3470598ad7ecd675f4072881ebd7d45554d7bb
cp <cp502966686@outlook.com>
2018-07-06 10:07:54 +0800

    Update:改进三数之和算法，新增btree。

---
+++ BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,13 @@
+package BinarySearchTree;
+
+public class BinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return false;
+    }
+
+    public static void main(String[] args){
+        TreeNode bt = new TreeNode(5);
+        System.out.println(bt);
+    }
+}
---
+++ BinarySearchTree/TreeNode.java
@@ -0,0 +1,16 @@
+package BinarySearchTree;
+
+
+/**
+ * Definition for a binarytree node.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
