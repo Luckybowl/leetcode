4f38f55822b64c0bfb8169860448e9f67d86563b
cp <cp502966686@outlook.com>
2018-07-06 11:42:39 +0800

    Update:更换算法，提升效率。

--- SumOfMinMax.java
+++ SumOfMinMax.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 三数之和
  * *给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
@@ -20,7 +22,9 @@ public class SumOfMinMax {
         if (nums.length % 2 != 0) {
             throw new IllegalArgumentException("Illegal nums!");
         }
-        nums = QuickSort.quickSort(nums);
+        //用容器自带的方法，速度提升很大
+        Arrays.sort(nums);
+//        nums = QuickSort.quickSort(nums);
 //        nums = SumOfTwoNum.BubbleSort(nums);
         /*for(int i = 0 ; i < nums.length ; i ++){
             for(int j = i+1 ; j < nums.length; j++ ){
