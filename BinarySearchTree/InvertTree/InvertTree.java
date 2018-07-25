c2e17f81667562017baf93c98b6eb750e584df1e
cp <cp502966686@outlook.com>
2018-07-25 17:48:33 +0800

    226. 翻转二叉树

---
+++ BinarySearchTree/InvertTree/InvertTree.java
@@ -0,0 +1,50 @@
+package BinarySearchTree.InvertTree;
+
+import BinarySearchTree.TreeNode;
+
+/**
+ *
+ * 226. 翻转二叉树
+ *
+ *翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ *
+ */
+
+public class InvertTree {
+    TreeNode temp;
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+
+    public void invert(TreeNode root){
+        if(root == null){
+            return ;
+        }
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invert(root.left);
+        invert(root.right);
+    }
+
+}
--- BinarySearchTree/TreeNode.java
+++ BinarySearchTree/TreeNode.java
@@ -6,10 +6,10 @@ package BinarySearchTree;
  */
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    public TreeNode left;
+    public TreeNode right;
 
-    TreeNode(int x) {
+    public TreeNode(int x) {
         val = x;
     }
 
