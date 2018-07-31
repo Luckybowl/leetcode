30659924d53467e507c1bf4f0d991ddeee12db19
cp <cp502966686@outlook.com>
2018-07-31 09:50:03 +0800

    2. 两数相加(中等)

---
+++ AddTwoNums/AddTwoNums.java
@@ -0,0 +1,109 @@
+package AddTwoNums;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 2. 两数相加(中等)
+ * <p>
+ * 给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+ * <p>
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ */
+public class AddTwoNums {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode ls;
+//        count(l1, l2, ls, 0);
+          ListNode dummyHead = new ListNode(0);
+          ListNode p = l1;
+          ListNode q = l2;
+          ListNode curr = dummyHead;
+          int carry = 0;
+          while(p != null || q != null){
+              int x = (p != null) ? p.val : 0;
+              int y = (q != null) ? q.val : 0;
+              int sum = x + y + carry;
+              carry = sum /10 ;
+              curr.next = new ListNode(sum % 10);
+              curr = curr.next;
+              if(p != null) p = p.next;
+              if(q != null) q = q.next;
+          }
+          if(carry > 0){
+              curr.next = new ListNode(carry);
+          }
+        return dummyHead.next;
+    }
+
+    public ListNode getNext(ListNode ls) {
+        return ls.next;
+    }
+
+
+    /**
+     * 第一次思索，使用递归，结果只能将其存入list中，无法放入ListNode中，故放弃。
+     *
+     * @param l1
+     * @param l2
+     * @param ls
+     * @param left
+     */
+    @Deprecated
+    public void count(ListNode l1, ListNode l2, ListNode ls, int left) {
+//        while (getNext(ls) != null) {
+//            ls = ls.next;
+//        }
+        if (l1 == null && l2 == null) {
+            if (left == 0) {
+                return;
+            } else {
+                ls = new ListNode(1);
+                return;
+            }
+        }
+        if (l1 != null && l2 != null) {
+            if (l1.val + l2.val >= 10) {
+                ls = new ListNode(l1.val + l2.val - 10 + left);
+                left = 1;
+                count(l1.next, l2.next, ls, left);
+            } else {
+                ls = new ListNode(l1.val + l2.val + left);
+                left = 0;
+                count(l1.next, l2.next, ls, left);
+            }
+        } else {
+            ListNode temp = null != l1 ? l1 : l2;
+            if (temp.val + left >= 10) {
+                ls = new ListNode(temp.val + left - 10);
+                left = 1;
+                count(l1.next, l2.next, ls, left);
+            } else {
+                ls = new ListNode(temp.val + left);
+                left = 0;
+                count(l1.next, l2.next, ls, left);
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        AddTwoNums addTwoNums = new AddTwoNums();
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        System.out.println(addTwoNums.addTwoNumbers(l1, l2));
+
+    }
+}
+
+
