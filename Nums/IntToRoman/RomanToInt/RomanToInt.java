bbbe6707c1928e9a3304736119abdb8a20cc9935
chenpeng <chenpeng@primeton.com>
2018-08-03 14:36:00 +0800

    13. 罗马数字转整数(简单)

---
+++ Nums/IntToRoman/RomanToInt/RomanToInt.java
@@ -0,0 +1,83 @@
+package Nums.IntToRoman.RomanToInt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 13. 罗马数字转整数(简单)
+ * <p>
+ * 罗马数字包含以下七种字符：I， V， X， L，C，D 和 M。
+ * <p>
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+ * <p>
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+ * <p>
+ * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。
+ * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ * 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "III"
+ * 输出: 3
+ * 示例 2:
+ * <p>
+ * 输入: "IV"
+ * 输出: 4
+ * 示例 3:
+ * <p>
+ * 输入: "IX"
+ * 输出: 9
+ * 示例 4:
+ * <p>
+ * 输入: "LVIII"
+ * 输出: 58
+ * 解释: C = 100, L = 50, XXX = 30, III = 3.
+ * 示例 5:
+ * <p>
+ * 输入: "MCMXCIV"
+ * 输出: 1994
+ * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+ * <p>
+ * <p>
+ * 基本思想是根据罗马数字的特征，即左加右减的规律， 比如IX = 9， XI =11
+ */
+public class RomanToInt {
+    public int romanToInt(String s) {
+        char[] c = s.toCharArray();
+        int temp = 0;
+        int sum = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        for (int i = c.length - 1; i >= 0; i--) {
+            if (temp > map.get(c[i])) {
+                sum -= map.get(c[i]);
+            } else {
+                sum += map.get(c[i]);
+            }
+            temp = map.get(c[i]);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        RomanToInt rt = new RomanToInt();
+        String s = "III";
+        System.out.print(rt.romanToInt(s));
+    }
+}
